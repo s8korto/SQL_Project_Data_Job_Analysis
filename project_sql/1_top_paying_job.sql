d2fe07845efb3edecc6fe4c8451f3134ab1fe4dd
s8korto <phongtran935@gmail.com>
2025-11-09 14:43:00 +1100

    Adding sql project that I forgot to add

--- project_sql/1_top_paying_job.sql
+++ project_sql/1_top_paying_job.sql
@@ -1,1 +1,27 @@
--- delete this later fggrg
+/* 
+Top 20 Highest Paying Data Scientist Jobs
+This query retrieves the top 50 highest paying Data Scientist job postings
+Focues on job title with salary information available
+Why? Aim to highlight top paying opportunities for Data Scientists, offering insights into employment options and location flexibility
+*/
+
+--- Calculate top paying Data Scientist jobs with respective skills companies are looking for
+SELECT 
+    job_postings_fact.job_id,
+    company_dim.name AS company_name,
+    job_postings_fact.job_title,
+    job_postings_fact.job_location,
+    job_postings_fact.job_schedule_type,
+    job_postings_fact.salary_year_avg,
+    job_postings_fact.job_posted_date
+FROM 
+    job_postings_fact
+LEFT JOIN company_dim 
+    ON job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_postings_fact.salary_year_avg IS NOT NULL 
+    AND job_postings_fact.job_title = 'Data Scientist'
+ORDER BY 
+    job_postings_fact.salary_year_avg DESC
+LIMIT 20;
+
---
+++ project_sql/2_top_paying_job.sql
@@ -0,0 +1,40 @@
+/* 
+Identify top highest paying Data Scientist jobs along with required skills
+Why? It provide a detailed on job in demand skills for high paying Data Scientist roles, helping job seekers align their skillset with market needs
+*/
+
+-- Calculate top paying Data Scientist jobs with respective skills companies are looking for
+WITH top_paying_jobs AS (
+SELECT 
+    job_postings_fact.job_id,
+    company_dim.name AS company_name,
+    job_postings_fact.job_title,
+    job_postings_fact.salary_year_avg
+FROM 
+    job_postings_fact
+LEFT JOIN company_dim 
+    ON job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_postings_fact.salary_year_avg IS NOT NULL 
+    AND job_postings_fact.job_title = 'Data Scientist'
+ORDER BY 
+    job_postings_fact.salary_year_avg DESC
+)
+-- List skills associated with these top paying jobs
+SELECT 
+    top_paying_jobs.job_id,
+    top_paying_jobs.company_name,
+    top_paying_jobs.job_title,
+    skills_dim.skills
+FROM   
+    top_paying_jobs
+LEFT JOIN skills_job_dim
+    ON top_paying_jobs.job_id = skills_job_dim.job_id
+LEFT JOIN skills_dim
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 50;
+
+
+
